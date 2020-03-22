082c43ad409009efbc1bd43f698a870e57ab0c1b
david <david.ber.1530@gmail.com>
2020-03-22 16:14:43 -0300

    prueba de la funcion Red

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 (function(w) {
   w.onload = function() {
     console.log("la pagina se cargo");
-    Red("https://swapi.co/api/people/")
-      .then(resul => console.log(resul))
-      .catch(err => console.log(err));
+    Red("https://swapi.co/api/people/").then(resul => console.log(resul));
   };
 }.call(null, window));
