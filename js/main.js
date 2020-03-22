b8d6b8c08b6ef359e50869e2e42c32e0930cdcc7
david <david.ber.1530@gmail.com>
2020-03-22 16:11:08 -0300

    se hicieron arreglos en la funcion Red

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 (function(w) {
   w.onload = function() {
     console.log("la pagina se cargo");
-    Red("https://swapi.co/api/people/", resuls => console.log(resuls));
+    Red("https://swapi.co/api/people/")
+      .then(resul => console.log(resul))
+      .catch(err => console.log(err));
   };
 }.call(null, window));
--- js/promis.js
+++ js/promis.js
@@ -1,10 +1,10 @@
-const Red = function(url, callback) {
+const Red = function(url) {
   return new Promise((result, reject) => {
     const XHTTP = new XMLHttpRequest();
     XHTTP.onreadystatechange = function() {
-      if (XHTTP.readyState === 4 && XHTTP.status === 200) {
-        callback(JSON.parse(this.response));
-      }
+      if (XHTTP.readyState === 4 && XHTTP.status === 200)
+        result(JSON.parse(this.response));
+      else if (XHTTP.status === 404) reject(new Error("page not found"));
     };
     XHTTP.open("GET", url, true);
     XHTTP.send();
